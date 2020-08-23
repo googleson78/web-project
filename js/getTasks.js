a8d1484412f825b7dbc18986816be5ccac352907
Parashkev Katerski <pkaterski@gmail.com>
2020-08-23 22:41:18 +0300

    show errors for task viewing

--- js/getTasks.js
+++ js/getTasks.js
@@ -1,31 +1,38 @@
 import { getApiTask } from './queries.js'
 
 function generateHTMLList(tasks) {
-  let res = '';
+  let res = ''
   for (let item of tasks) {
-    const task = item.task;
-    res +=
-      `
+    const task = item.task
+    res += `
       <div class="panel panel-default">
         <div class="panel-heading"><a href="#">${task.name}</a></div>
         <div class="panel-body">
           <p>Language: ${task.language}</p>
           <p>Description: ${task.description}</p>
         </div>
-      </div>`;
+      </div>`
   }
-  return res;
+  return res
+}
+
+function generateHTMLError(err) {
+  return `<div class="panel panel-danger">
+            <div class="panel-heading">An Error Occured</div>
+            <div class="panel-body">
+              <p>Error: ${err}</p>
+            </div>
+          </div>`
 }
 
 function getTasks() {
   getApiTask(
     '',
     (tasks) =>
-      (document.getElementById('tasks').innerHTML
-        = generateHTMLList(tasks)
-      ),
-    (err) => alert(err),
+      (document.getElementById('tasks').innerHTML = generateHTMLList(tasks)),
+    (err) =>
+      (document.getElementById('tasks').innerHTML = generateHTMLError(err)),
   )
 }
 
-getTasks();
+getTasks()
