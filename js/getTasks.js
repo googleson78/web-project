c2152e6e3a11495c998a768d2212a52116867e65
Parashkev Katerski <pkaterski@gmail.com>
2020-08-23 22:04:40 +0300

    show tasks list in tasks.php

--- js/getTasks.js
+++ js/getTasks.js
@@ -1,12 +1,31 @@
 import { getApiTask } from './queries.js'
 
+function generateHTMLList(tasks) {
+  let res = '';
+  for (let item of tasks) {
+    const task = item.task;
+    res +=
+      `
+      <div class="panel panel-default">
+        <div class="panel-heading"><a href="#">${task.name}</a></div>
+        <div class="panel-body">
+          <p>Language: ${task.language}</p>
+          <p>Description: ${task.description}</p>
+        </div>
+      </div>`;
+  }
+  return res;
+}
+
 function getTasks() {
   getApiTask(
     '',
     (tasks) =>
-      (document.getElementById('tasks').innerHTML = JSON.stringify(tasks)),
+      (document.getElementById('tasks').innerHTML
+        = generateHTMLList(tasks)
+      ),
     (err) => alert(err),
   )
 }
 
-window.getTasks = getTasks // TODO: don't use globals
+getTasks();
