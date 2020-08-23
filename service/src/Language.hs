cfcca55ddcb70cd8c72ef087cf4fb10bc6a16070
Georgi Lyubenov <godzbanebane@gmail.com>
2020-08-23 20:38:01 +0300

    Encode Language as a string
    It's currently [], because it only has one constructor

--- service/src/Language.hs
+++ service/src/Language.hs
@@ -5,12 +5,18 @@ module Language
   ) where
 
 import GHC.Generics (Generic)
-import Data.Aeson (ToJSON, FromJSON)
+import Data.Aeson (genericParseJSON, genericToJSON, tagSingleConstructors, ToJSON(..), FromJSON(..), defaultOptions)
 import Database.Persist.TH (derivePersistFieldJSON)
 
 data Language
   = Racket
   deriving stock (Show, Eq, Generic)
-  deriving anyclass (ToJSON, FromJSON)
+
+-- TODO: these can just be anyclass, once there is more than one 'Language' constructor
+instance ToJSON Language where
+  toJSON = genericToJSON defaultOptions {tagSingleConstructors = True}
+
+instance FromJSON Language where
+  parseJSON = genericParseJSON defaultOptions {tagSingleConstructors = True}
 
 derivePersistFieldJSON "Language"
