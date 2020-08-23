823f68f1bf76e96a2f484a3a308ba67788299f95
Georgi Lyubenov <godzbanebane@gmail.com>
2020-08-23 21:57:35 +0300

    Center the add a task page

--- php/submit/task.php
+++ php/submit/task.php
@@ -36,44 +36,46 @@ session_start();
 <body>
     <?php require_once '../navbar.php'; ?>
 
-    
-    <form action="javascript:;" onsubmit="addTask()">
-      <div class="form-group">
-        <label>Task name
-          <input class="form-control" name="name" id="name"></input>
-        </label>
-      </div>
 
-      <div class="form-group">
-        <label>Task description
-          <input class="form-control" name="description" id="description"></input>
-        </label>
-      </div>
+    <div class="container">
+      <form action="javascript:;" onsubmit="addTask()">
+        <div class="form-group">
+          <label>Task name
+            <input class="form-control" name="name" id="name"></input>
+          </label>
+        </div>
 
-      <div class="form-group">
-        <label>Expected filename for task solution - tests are expected to "import" this file
-          <input class="form-control" name="expected-filename" id="expected-filename"></input>
-        </label>
-      </div>
+        <div class="form-group">
+          <label>Task description
+            <input class="form-control" name="description" id="description"></input>
+          </label>
+        </div>
 
-      <div class="form-group">
-        <label>Task programming language
-          <select class="form-control" name="language" id="language">
-            <option value="Racket">Racket</option>
-          </select>
-        </label>
-      </div>
+        <div class="form-group">
+          <label>Expected filename for task solution - tests are expected to "import" this file
+            <input class="form-control" name="expected-filename" id="expected-filename"></input>
+          </label>
+        </div>
 
-      <div class="form-group">
-        <label>Tests code
-          <textarea class="form-control" rows="6" cols="80" name="tests" id="tests"></textarea>
-        </label>
-      </div>
+        <div class="form-group">
+          <label>Task programming language
+            <select class="form-control" name="language" id="language">
+              <option value="Racket">Racket</option>
+            </select>
+          </label>
+        </div>
 
-      <div class="form-group">
-        <input type="submit" value="Submit">
-      </div>
-    </form>
+        <div class="form-group">
+          <label>Tests code
+            <textarea class="form-control" rows="6" cols="80" name="tests" id="tests"></textarea>
+          </label>
+        </div>
+
+        <div class="form-group">
+          <input type="submit" value="Submit">
+        </div>
+      </form>
+    </div>
 
 </body>
 </html>
