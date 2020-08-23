401b80162fb8f8fc73fb72bb743eb2c0e8f29b3b
Parashkev Katerski <pkaterski@gmail.com>
2020-08-23 14:57:05 +0300

    add skeleton

---
+++ js/getTasks.js
@@ -0,0 +1,14 @@
+import { getApiTask } from './queries.js'
+
+alert('This alert box was called with the onload event')
+
+function getTasks() {
+  getApiTask(
+    '',
+    (tasks) =>
+      (document.getElementById('tasks').innerHTML = JSON.stringify(tasks)),
+    (err) => alert(err),
+  )
+}
+
+window.getTasks = getTasks // TODO: don't use globals
