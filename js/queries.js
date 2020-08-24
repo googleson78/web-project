d621428dc84e00896a01418c9ef27c5ad326e74d
Georgi Lyubenov <godzbanebane@gmail.com>
2020-08-24 10:49:19 +0300

    Generate js for GetAllSubmissions

--- js/queries.js
+++ js/queries.js
@@ -82,4 +82,25 @@ var postApiSubmit = function(body, onSuccess, onError) {
   xhr.send(JSON.stringify(body));
 };
 
-export { getApiTask, postApiTask, getApiTasks, postApiSubmit }
+var getApiSubmissions = function(taskID, headerCookie, onSuccess, onError) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/api/submissions' + '?taskID=' + encodeURIComponent(taskID), true);
+  xhr.withCredentials = true;
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function () {
+    var res = null;
+    if (xhr.readyState === 4) {
+      if (xhr.status === 204 || xhr.status === 205) {
+        onSuccess();
+      } else if (xhr.status >= 200 && xhr.status < 300) {
+        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
+        if (res) onSuccess(res);
+      } else {
+        if (xhr.responseText) onError(xhr.responseText)
+      }
+    }
+  };
+  xhr.send(null);
+};
+
+export { getApiTask, postApiTask, getApiTasks, postApiSubmit, getApiSubmissions }
